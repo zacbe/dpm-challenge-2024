f7ecaa862d545eacf5b50037a286e70ffdf3f98f
@zacbe <zacbegonzalez@gmail.com>
2024-05-29 19:01:46 +0200

    fix: lint

--- audio-recording-app/app/components/RecordingView.tsx
+++ audio-recording-app/app/components/RecordingView.tsx
@@ -69,7 +69,7 @@ export default function RecordingView() {
     try {
       // TODO: Replace example.com with API endpoint
       // add as environment variable???
-      const response = await axios.post(`https://webhook.site/e702dfcd-b9f7-49f8-9ed7-a957961d7ad6`, formData,{
+      const response = await axios.post(`https://webhook.site/e702dfcd-b9f7-49f8-9ed7-a957961d7ad6`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -77,6 +77,9 @@ export default function RecordingView() {
       console.log("Upload successful:", response.data)
     } catch (error) {
       console.error("Error uploading audio:", error)
+    } finally {
+      setRecording(null)
+      setAudioURL(null)
     }
   }
 
