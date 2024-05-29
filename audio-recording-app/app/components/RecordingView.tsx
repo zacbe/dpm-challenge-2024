01249f14e666d72b4e1dfc5ea025e8232cd8523a
@zacbe <zacbegonzalez@gmail.com>
2024-05-29 18:55:25 +0200

    chore: Set correct headers

--- audio-recording-app/app/components/RecordingView.tsx
+++ audio-recording-app/app/components/RecordingView.tsx
@@ -69,7 +69,11 @@ export default function RecordingView() {
     try {
       // TODO: Replace example.com with API endpoint
       // add as environment variable???
-      const response = await axios.post(`https://webhook.site/e702dfcd-b9f7-49f8-9ed7-a957961d7ad6`, formData)
+      const response = await axios.post(`https://webhook.site/e702dfcd-b9f7-49f8-9ed7-a957961d7ad6`, formData,{
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
       console.log("Upload successful:", response.data)
     } catch (error) {
       console.error("Error uploading audio:", error)
