734a634c9f82c2c370f0ff70253fcbdba3f1d495
@zacbe <zacbegonzalez@gmail.com>
2024-05-29 19:37:34 +0200

    chore: Add rabbitMQService module for connecting to RabbitMQ and publishing messages to queues

---
+++ audio-service/src/services/rabbitMQService.ts
@@ -0,0 +1,33 @@
+import amqp from 'amqplib';
+import { env } from '../utils/envConfig';
+
+let connection: amqp.Connection;
+let channel: amqp.Channel;
+
+const JOB_QUEUE = env.JOB_QUEUE;
+const COMPLETION_QUEUE = env.COMPLETION_QUEUE;
+const RABBITMQ_URL = env.RABBITMQ_URL;
+
+export const connectRabbitMQ = async (): Promise<void> => {
+  try {
+    connection = await amqp.connect(RABBITMQ_URL);
+    channel = await connection.createChannel();
+    await channel.assertQueue(JOB_QUEUE, { durable: true });
+    await channel.assertQueue(COMPLETION_QUEUE, { durable: true });
+  } catch (error) {
+    console.error('Failed to connect to RabbitMQ:', error);
+    throw error;
+  }
+};
+
+
+export const publishToQueue = async (queue: string, message: string) => {
+  if (!channel) {
+    throw new Error('Channel is not created. Please connect to RabbitMQ first.');
+  }
+  channel.sendToQueue(queue, Buffer.from(message), { persistent: true });
+};
+
+
+
+
