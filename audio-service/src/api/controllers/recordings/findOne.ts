5757d730862fde6a2a2b681d5c0e3c44d16b158d
@zacbe <zacbegonzalez@gmail.com>
2024-05-29 19:17:42 +0200

    chore: Add http-errors dependency and handle errors in findOne and uploadOne controllers

--- audio-service/src/api/controllers/recordings/findOne.ts
+++ audio-service/src/api/controllers/recordings/findOne.ts
@@ -1,5 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
 
 export default async function handler(req: Request, res: Response, next: NextFunction): Promise<void> {
-  res.status(200)
+
+  try {
+    const recording = {} // find the recording in the database
+    if (!recording) return next(createError(404, 'Recording not found'));
+    
+    res.send(recording);
+  } catch (e: any) {
+    next(createError(500, e.message));
+  }
 }
--- audio-service/src/api/controllers/recordings/uploadOne.ts
+++ audio-service/src/api/controllers/recordings/uploadOne.ts
@@ -1,5 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
 
 export default async function handler(req: Request, res: Response, next: NextFunction): Promise<void> {
-  res.status(200)
+  try {
+    const body = {
+      recording: "file.buffer",
+      user_email: "userEmail"
+    }
+
+    const recording = {} // insert the recording into the database
+    if (!recording) return next(createError(400, 'Error creating recording'));
+
+    res.status(200).send('File received and will be processed.');
+  } catch (e: any) {
+    next(createError(500, e.message));
+  }
 }
