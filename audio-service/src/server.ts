79f04bcee026fe0c8d638d262eb50091790df9c2
@zacbe <zacbegonzalez@gmail.com>
2024-06-05 17:56:57 +0200

    chore: Add SSE endpoint for real-time event streaming

--- audio-service/src/server.ts
+++ audio-service/src/server.ts
@@ -5,20 +5,41 @@ import express, { Express, Response, Request } from "express";
 import { recordingRouter } from './api/routes';
 import { errorHandler } from './middleware';
 import { sequelize } from './models';
-import { connectRabbitMQ } from './services';
+import { connectRabbitMQ, consumeCompletionNotifications } from './services';
 import { env } from './utils/envConfig';
+
+const clients: Response[] = [];
+const eventsHandler = (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders();
+
+  clients.push(res);
+
+  req.on('close', () => {
+    clients.splice(clients.indexOf(res), 1);
+  });
+};
 
 
 (async () => {
-  // initialize the squelize connection
+  // Initialize the Sequelize connection
   await sequelize.sync({ alter: true, force: false });
   connectRabbitMQ()
-  .then(() => console.log('Connected to RabbitMQ'))
-  .catch((err) => {
-    console.error('Failed to connect to RabbitMQ', err);
-    process.exit(1);
-  });
+    .then(() => {
+      console.log('Connected to RabbitMQ');
+      consumeCompletionNotifications(notifyClients);
+    })
+    .catch((err) => {
+      console.error('Failed to connect to RabbitMQ', err);
+      process.exit(1);
+    });
 })();
+
+const notifyClients = (message: string) => {
+  clients.forEach(client => client.write(`data: ${message}\n\n`));
+};
 
 const corsOptions = {
   origin: env.CORS_ORIGIN,
@@ -34,6 +55,9 @@ app.use(bodyParser.json());
 // Sequelize
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
+
+// SSE endpoint
+app.get('/events', eventsHandler);
 
 // Routes
 app.use('/api/v1', recordingRouter);
